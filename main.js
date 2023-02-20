22f849ce4263922a6543e675afcad6daa9c691b3
Ivan Adamo <ivan.adamo.8@gmail.com>
2023-02-20 02:36:28 -0300

    logica inicial planteada

--- main.js
+++ main.js
@@ -10,55 +10,92 @@ const person1 = document.querySelector('#person1');
 const person2 = document.querySelector('#person2');
 const person3 = document.querySelector('#person3');
 const person4 = document.querySelector('#person4');
+const saldos1 = [];
+const saldos2 = [];
+const saldos3 = [];
+const saldos4 = [];
+let total1 = 0;
+let total2 = 0;
+let total3 = 0;
+let total4 = 0;
+//let total1 = saldos1.reduce((a, b) => a + b, 0);
 
 btn1.addEventListener('click', botonClick1);
 btn2.addEventListener('click', botonClick2);
 btn3.addEventListener('click', botonClick3);
 btn4.addEventListener('click', botonClick4);
 
+function calcularSaldos() {
+  total1 = saldos1.reduce((a, b) => a + b, 0);
+  total2 = saldos2.reduce((a, b) => a + b, 0);
+  total3 = saldos3.reduce((a, b) => a + b, 0);
+  total4 = saldos4.reduce((a, b) => a + b, 0);
+}
+
+function mostrarResultados() {
+  person1.innerText = "$ " + total1;
+  person2.innerText = "$ " + total2;
+  person3.innerText = "$ " + total3;
+  person4.innerText = "$ " + total4;
+}
+
+function mostrarEnConsola() {
+  console.log(saldos1, saldos2, saldos3, saldos4);
+  console.log(total1, total2, total3, total4);
+}
+
 function botonClick1() {
   let number1 = Number(input1.value);
 
   console.log('funciona!');
 
-  const suma = number1 / 4;
-
-  person1.innerText = "$ " + suma;  
-  person2.innerText = "$ " + suma;  
-  person3.innerText = "$ " + suma;  
-  person4.innerText = "$ " + suma;  
+  saldos1.push(number1 - (number1 * 1.75));
+  saldos2.push(number1 * 0.25);
+  saldos3.push(number1 * 0.25);
+  saldos4.push(number1 * 0.25);
+  calcularSaldos();
+  mostrarResultados();
+  mostrarEnConsola();
 }
 
 function botonClick2() {
-  
-  let number2 = Number(input2.value);
-  
+  let number2 = Number(input1.value);
+
   console.log('funciona!');
-  const suma = number2 / 4;
-  person1.innerText = "$ " + suma;  
-  person2.innerText = "$ " + suma;  
-  person3.innerText = "$ " + suma;  
-  person4.innerText = "$ " + suma;  
+
+  saldos1.push(number2 * 0.25);
+  saldos2.push(number2 - (number2 * 1.75));
+  saldos3.push(number2 * 0.25);
+  saldos4.push(number2 * 0.25);
+  calcularSaldos();
+  mostrarResultados();
+  mostrarEnConsola();
 }
 
 function botonClick3() {
   let number3 = Number(input3.value);
 
   console.log('funciona!');
-  const suma = number3 / 4;
-  person1.innerText = "$ " + suma;  
-  person2.innerText = "$ " + suma;  
-  person3.innerText = "$ " + suma;  
-  person4.innerText = "$ " + suma;  
+  
+  saldos1.push(number3 * 0.25);
+  saldos2.push(number3 * 0.25);
+  saldos3.push(number3 - (number3 * 1.75));
+  saldos4.push(number3 * 0.25);
+  calcularSaldos();
+  mostrarResultados();
+  mostrarEnConsola();
 }
 
 function botonClick4() {
 
   let number4 = Number(input4.value);
   console.log('funciona!');
-  const suma = number4 / 4;
-  person1.innerText = "$ " + suma;  
-  person2.innerText = "$ " + suma;  
-  person3.innerText = "$ " + suma;  
-  person4.innerText = "$ " + suma;  
+  
+  saldos1.push(number4 * 0.25);
+  saldos2.push(number4 * 0.25);
+  saldos3.push(number4 * 0.25);
+  saldos4.push(number4 - (number4 * 1.75));
+  calcularSaldos();
+  mostrarResultados();
+  mostrarEnConsola();
 }
