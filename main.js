a07c0de71f59fe6d50420aba146dcc1f00dd4898
Ivan Adamo <ivan.adamo.8@gmail.com>
2023-02-20 01:35:31 -0300

    added inputs, buttons and outputs

--- main.js
+++ main.js
@@ -1,14 +1,64 @@
-const form = document.querySelector('#form')
 const input1 = document.querySelector('#calculo1');
 const input2 = document.querySelector('#calculo2');
-const btn = document.querySelector('#btn-calcular');
-const result = document.querySelector('#result');
+const input3 = document.querySelector('#calculo3');
+const input4 = document.querySelector('#calculo4');
+const btn1 = document.querySelector('#btn-calcular1');
+const btn2 = document.querySelector('#btn-calcular2');
+const btn3 = document.querySelector('#btn-calcular3');
+const btn4 = document.querySelector('#btn-calcular4');
+const person1 = document.querySelector('#person1');
+const person2 = document.querySelector('#person2');
+const person3 = document.querySelector('#person3');
+const person4 = document.querySelector('#person4');
 
-btn.addEventListener('click', botonClick)
+btn1.addEventListener('click', botonClick1);
+btn2.addEventListener('click', botonClick2);
+btn3.addEventListener('click', botonClick3);
+btn4.addEventListener('click', botonClick4);
 
-function botonClick(event) {
-   // console.log({event})
-   // event.preventDefault()
-    const suma = input1.value + input2.value;
-    result.innerText = "Resultado: " + suma;  
+function botonClick1() {
+  let number1 = Number(input1.value);
+
+  console.log('funciona!');
+
+  const suma = number1 / 4;
+
+  person1.innerText = "$ " + suma;  
+  person2.innerText = "$ " + suma;  
+  person3.innerText = "$ " + suma;  
+  person4.innerText = "$ " + suma;  
+}
+
+function botonClick2() {
+  
+  let number2 = Number(input2.value);
+  
+  console.log('funciona!');
+  const suma = number2 / 4;
+  person1.innerText = "$ " + suma;  
+  person2.innerText = "$ " + suma;  
+  person3.innerText = "$ " + suma;  
+  person4.innerText = "$ " + suma;  
+}
+
+function botonClick3() {
+  let number3 = Number(input3.value);
+
+  console.log('funciona!');
+  const suma = number3 / 4;
+  person1.innerText = "$ " + suma;  
+  person2.innerText = "$ " + suma;  
+  person3.innerText = "$ " + suma;  
+  person4.innerText = "$ " + suma;  
+}
+
+function botonClick4() {
+
+  let number4 = Number(input4.value);
+  console.log('funciona!');
+  const suma = number4 / 4;
+  person1.innerText = "$ " + suma;  
+  person2.innerText = "$ " + suma;  
+  person3.innerText = "$ " + suma;  
+  person4.innerText = "$ " + suma;  
 }
